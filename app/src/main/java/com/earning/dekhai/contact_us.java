67e9867c0464afac91db71c20e091e9351b0e578
wasiul-mannan <amd.wasiulmannan@gmail.com>
2021-12-12 20:33:46 +0600

    design 90% done

---
+++ app/src/main/java/com/earning/dekhai/contact_us.java
@@ -0,0 +1,14 @@
+package com.earning.dekhai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contact_us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_us_activity);
+    }
+}
