57e7bb306ecfc10e6ec5eb4bafafcd829535b376
wasiul-mannan <amd.wasiulmannan@gmail.com>
2021-12-16 22:42:57 +0600

    LearningPageAllDesignDone

---
+++ app/src/main/java/com/earning/dekhai/LanguageLearningPage.java
@@ -0,0 +1,14 @@
+package com.earning.dekhai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LanguageLearningPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.language_learning_page_activity);
+    }
+}
